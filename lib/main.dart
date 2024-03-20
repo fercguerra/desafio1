69f6579166cc0cb1e25899b9c201920be4465a69
fercguerra <fercguerra06@gmail.com>
2024-03-20 15:00:40 -0300

    Botões

--- lib/main.dart
+++ lib/main.dart
@@ -7,33 +7,38 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text(
-            'Get your Money',
-            style: TextStyle(fontSize: 24,
-            fontWeight: FontWeight.bold),
-          ),
-          Text(
-            'Under Control',
-            style: TextStyle(fontSize: 24,
-            fontWeight: FontWeight.bold),
-          ),
-          Text(
-            'Manage your expenses.'
-          ),
-          Text(
-            'Seamlessly.'
-          ),
-        
-
-          
-              // Outros widgets podem ser adicionados aqui
+              const Text(
+                'Get your Money',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+              const Text(
+                'Under Control',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+              const Text('Manage your expenses.'),
+              const Text('Seamlessly.'),
+              SizedBox(height: 20), // Adiciona um espaço entre o texto e o botão
+              ElevatedButton(
+                onPressed: () {
+                  // Adicione a ação do botão aqui
+                },
+                child: Text('Sign Up with Email ID'),
+              ),
+              SizedBox(height: 20), // Adiciona um espaço entre o texto e o botão
+              ElevatedButton(
+              
+                onPressed: () {
+                  // Adicione a ação do botão aqui
+                },
+                child: Text('Sign Up with Google'),
+              ),
             ],
           ),
         ),
@@ -41,6 +46,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-
-      

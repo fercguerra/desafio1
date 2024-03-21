ed5a9665f9422a17a055783a324ec8f298b8e940
fercguerra <fercguerra06@gmail.com>
2024-03-21 10:34:08 -0300

    Alterações

--- lib/main.dart
+++ lib/main.dart
@@ -10,26 +10,37 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+         backgroundColor: Colors.black,
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               const Text(
                 'Get your Money',
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.white),
               ),
               const Text(
                 'Under Control',
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.white),
               ),
-              const Text('Manage your expenses.'),
-              const Text('Seamlessly.'),
+              const Text('Manage your expenses.', style: TextStyle(color: Colors.grey),),
+              const Text('Seamlessly.', style: TextStyle(color: Colors.grey),),
               SizedBox(height: 20), // Adiciona um espaço entre o texto e o botão
               ElevatedButton(
                 onPressed: () {
                   // Adicione a ação do botão aqui
                 },
-                child: Text('Sign Up with Email ID'),
+                style: ElevatedButton.styleFrom(
+                  padding: EdgeInsets.symmetric(vertical: 10, horizontal:100 ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(5)
+                  ),
+                  backgroundColor: Color.fromARGB(255, 80, 82, 211), // Define o tamanho do botão
+                ),
+                
+                child: Text('Sign Up with Email ID',
+                style: TextStyle(color: Colors.white,),
+              ),
               ),
               SizedBox(height: 20), // Adiciona um espaço entre o texto e o botão
               IconButton(
